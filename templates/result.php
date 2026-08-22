c1da5de7b760094fc285872a979365162f1cb569
manhduong125 <phanduong125@gmail.com>
2026-08-22 20:58:43 +0700

    u

--- templates/result.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
-<div class="spin-wheel-result-content">
-    <h3><?php esc_html_e( 'Bạn đã trúng', 'wp-spin-wheel' ); ?></h3>
-    <p class="spin-wheel-result-title"></p>
-    <p class="spin-wheel-result-description"></p>
-</div>
